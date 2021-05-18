807a3538e98c759bde3f6760247363f3f1031ef9
Robert Chinem <robertchinem@gmail.com>
2021-05-17 22:22:08 -0300

    adicionando solução do problema troca - obi

---
+++ semana2-bixos2021/troca.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main(){
+    int n, t; cin >> n >> t;
+    vector<int> top(n), bottom(n);
+
+    for(int &t_i: top) cin >> t_i;
+    for(int &b_i: bottom) cin >> b_i;
+
+    vector<int> prefix_sum(n+10, 0);
+
+    while(t--){
+        int i, j; cin >> i >> j;
+        prefix_sum[i-1]++;
+        prefix_sum[j]--;
+    }
+
+    for(int i=1; i < prefix_sum.size(); i++){
+        prefix_sum[i] += prefix_sum[i-1];
+    }
+
+    for(int i=0; i < n; i++){
+        if(prefix_sum[i]%2 == 0) cout << top[i] << " ";
+        else cout << bottom[i] << " ";
+    }
+    
+    cout << endl;
+}
